f76bf7a7862c3b94cd0c32dc1a17981790c60cb3
Nic Wise <nicw@fastchicken.co.nz>
2016-01-17 18:32:02 +1300

    Project

---
+++ ProjectTemplate/ProjectTemplate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectTemplate
+//
+//  Created by Nic Wise on 17/01/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
