b52473dfd45fcea642ee1eb4f3baf383fb37e04e
Nic Wise <nicw@fastchicken.co.nz>
2016-01-17 18:31:52 +1300

    Adding Pods

---
+++ ProjectTemplate/Pods/Target Support Files/Valet/Valet-umbrella.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+#import "Valet.h"
+#import "VALSecureEnclaveValet.h"
+#import "VALSynchronizableValet.h"
+#import "VALValet.h"
+#import "VALValet_Protected.h"
+#import "ValetDefines.h"
+
+FOUNDATION_EXPORT double ValetVersionNumber;
+FOUNDATION_EXPORT const unsigned char ValetVersionString[];
+
